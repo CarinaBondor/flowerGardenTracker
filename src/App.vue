d80e3bbfb23809d7f0c6fab35ad40339dbedc680
CarinaBondor <carinabondor@gmail.com>
2026-09-01 23:34:16 +0300

    Wip base

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,30 @@
+<template>
+    <main>
+      <h1>{{ message }}</h1>
+
+      <taskForm/> 
+    </main>
+</template>
+
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
+import TaskForm from './components/taskForm.vue'
+
+const message = ref("flower-garden-tracker app");
+
+
 </script>
 
-<template>
-  <HelloWorld />
-</template>
+<style>
+main {
+  max-width: 800px;
+  margin: 1rem auto;
+}
+
+.button-container {
+  display: flex;
+  justify-content: end;
+  gap: 0.5rem;
+}
+</style>
+
---
+++ src/components/taskForm.vue
@@ -0,0 +1,24 @@
+<template>
+    <form @submit.prevent='formSubmitted'>
+        <label for="flowerName">Flower Name:</label>
+        <input type="text" id="flowerName" name="flowerName" v-model='newTask'/>
+        <div class="button-container">
+          <button type="submit">Add Flower</button>
+        </div>
+
+      </form>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const emit = defineEmits<{
+    addTask: [newTask: string]
+}>()
+
+const newTask = ref("");
+
+function formSubmitted( ){
+    console.log(newTask.value)
+}
+</script>
